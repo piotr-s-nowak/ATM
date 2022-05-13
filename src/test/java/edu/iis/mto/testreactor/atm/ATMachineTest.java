b0ac64e5d341b1f80808b802c300b7ac3bdde5a5
piotr-s-nowak <217728@edu.p.lodz.pl>
2022-05-13 15:23:29 +0200

    added test which should throw null pointer exception when currency is null

--- src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
+++ src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
@@ -1,19 +1,40 @@
 package edu.iis.mto.testreactor.atm;
 
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
 
-import org.hamcrest.Matchers;
+import edu.iis.mto.testreactor.atm.bank.AuthorizationException;
+import edu.iis.mto.testreactor.atm.bank.Bank;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Currency;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
 class ATMachineTest {
 
+    @Mock
+    private Bank bank;
+    private ATMachine atMachine;
+    private PinCode anyPin = PinCode.createPIN(1, 2, 3, 4);
+    private Card anyCard = Card.create("34985735");
+    private Money anyMoney = new Money(250, Currency.getInstance("PLN"));
+
     @BeforeEach
-    void setUp() throws Exception {}
+    public void setUp() {
+        atMachine = new ATMachine(bank, Currency.getInstance("PLN"));
+    }
 
     @Test
-    void itCompiles() {
-        assertThat(true, Matchers.equalTo(true));
+    public void shouldThrowNullPointerExceptionWhenCurrencyIsNull() {
+        assertThrows(NullPointerException.class, () -> new ATMachine(bank, null));
     }
 
 }
